dac8cbec4f4a775ae2b1d565204c1695054c4e48
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2023-01-19 19:44:49 +0000

    stuff

--- js/script.js
+++ js/script.js
@@ -7,17 +7,32 @@
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-01/sw.js", {
+    scope: "/ICS2O-Unit5-01/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function generates a random number between 1 and 6
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+const randomNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6 into variable "randomNumber"
+
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+
+  // process
+  if (valueFromSlider == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was " + randomNumber + " you got it!"
+  }
+
+  if (valueFromSlider != randomNumber) {
+    document.getElementById("answer").innerHTML = "Wrong answer. Try again"
+  }
 }
